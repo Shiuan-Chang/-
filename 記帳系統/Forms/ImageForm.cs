ce7acd7b60ad8ac5496a14c5181b4c738977bc71
MSI\icewi <icewindflyhero@gmail.com>
2024-09-20 18:28:01 +0800

    4th commit - collect memories when the image form colsed.

--- 記帳系統/Forms/ImageForm.cs
+++ 記帳系統/Forms/ImageForm.cs
@@ -14,6 +14,7 @@ namespace 記帳系統.Forms
     public partial class ImageForm : Form
     {
         private PictureBox pictureBox;
+        private Bitmap image;
         public ImageForm(string imagePath)
         {
             InitializeComponent();
@@ -34,12 +35,28 @@ namespace 記帳系統.Forms
         {
             if (System.IO.File.Exists(imagePath))
             {
-                this.pictureBox.Image = new Bitmap(imagePath);
+                this.image = new Bitmap(imagePath);
+                this.pictureBox.Image = this.image;
             }
             else
             {
                 MessageBox.Show("Image file not found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+
+        //覆寫基類（Form）的 OnFormClosed 方法，以便實現特定的清理或其他終止操作。
+        protected override void OnFormClosed(FormClosedEventArgs e) // onformcolsed是處理視窗關閉時觸發的事件
+        {
+            //將 PictureBox 的圖片返回null
+            pictureBox.Image = null;
+
+            //釋放圖檔
+            image?.Dispose();
+            image = null;
+
+            //進行回收
+            GC.Collect();
+            base.OnFormClosed(e);
         }
 
         private void ImageForm_Load(object sender, EventArgs e)
